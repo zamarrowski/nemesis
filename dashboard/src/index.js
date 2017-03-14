2a7bee3bb55737a9f6c7006b42ab45dd4cac84c8
zamarrowski <sergiozamarro@hotmail.com>
2017-03-14 22:31:10 +0100

    First commit

---
+++ dashboard/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import './index.css'
+import flexboxgrid from 'flexboxgrid'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
