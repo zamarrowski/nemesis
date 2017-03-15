43d4853af01f655a9112d6c8eab4288edd1d564f
zamarrowski <sergiozamarro@hotmail.com>
2017-03-15 23:18:14 +0100

    ReportCard component

--- dashboard/src/home/HomePage.js
+++ dashboard/src/home/HomePage.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import styled from 'styled-components'
+
+import ReportCard from './../reports/ReportCard'
+import lastReports from './lastReports.mock.js'
+
+const HomePageContainer = styled.div`
+  padding: 20px;
+`
+
+const ColReport = styled.div`
+  margin-top: 10px;
+`
 
 class HomePage extends React.Component {
   render() {
-    return(<h1>HomePage</h1>)
+    return(
+      <HomePageContainer>
+        <div className="row">
+            {lastReports.map((report, index) => (
+              <ColReport className="col-lg-4 col-md-6 col-xs-12" key={index}>
+                <ReportCard report={report} ></ReportCard>
+              </ColReport>
+            ))}
+        </div>
+      </HomePageContainer>
+    )
   }
 }
 
---
+++ dashboard/src/home/lastReports.mock.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    username: 'Sr. Zamarro',
+    feeling: 5,
+    comments: null,
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U1QFCH97X-05f4e73e4488-1024'
+  },
+  {
+    username: 'Sr. Zamarro',
+    feeling: 3,
+    comments: 'Ni bien ni mal.',
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U1QFCH97X-05f4e73e4488-1024'
+  },
+  {
+    username: 'Sr. Zamarro',
+    feeling: 1,
+    comments: 'El peor día de mi vida.',
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U1QFCH97X-05f4e73e4488-1024'
+  },
+  {
+    username: 'Alba Cañas',
+    feeling: 5,
+    comments: null,
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U0BDXTGQ3-07f047af98ba-1024'
+  },
+  {
+    username: 'Alba Cañas',
+    feeling: 2,
+    comments: null,
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U0BDXTGQ3-07f047af98ba-1024'
+  },
+  {
+    username: 'Alba Cañas',
+    feeling: 4,
+    comments: 'Un día genial :)',
+    date: new Date(),
+    photo: 'https://ca.slack-edge.com/T080Q0YJ0-U0BDXTGQ3-07f047af98ba-1024'
+  }
+]
