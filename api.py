1e64658f9130a113cf4101579fb9075b3e297bd2
Alba <alba.caon@gmail.com>
2017-03-16 19:38:46 +0100

    added gitignore, serialize fix, and api

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from bottle import route, run, template
+import json
+
+from bottle import route, run, template, response
 from models import HappyUserHistoric
 
+from mongoengine import connect
+
+connect('nemesis')
+
+response.content_type = 'application/json'
 
 @route('/last-reports/')
 def index():
-    return template('<b>Hello {{name}}</b>!', name=name)
+    reports = []
+    for happy in HappyUserHistoric.objects.all()[0:10]:
+        reports.append(happy.serialize())
+    return json.dumps(reports)
+
 
 run(host='localhost', port=8080)
--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import pytz
+
 from mongoengine import DynamicDocument, fields
 
 
@@ -10,18 +12,19 @@ class HappyUserHistoric(DynamicDocument):
     happy = fields.IntField(required=True)
     reported_at = fields.DateTimeField(required=True)
     comment = fields.StringField(required=False)
+    avatar = fields.StringField(required=False)
 
-    def serialize_reported_at(self):
+    def serialize_reported_at(self, timezone='Europe/Madrid'):
         current_tz = pytz.timezone(timezone)
-        timestamp = self.timestamp.replace(tzinfo=pytz.utc).astimezone(current_tz)
-        return '{datetime:%d-%m-%Y %H:%M:%S}'.format(datetime=timestamp)
+        reported_at = self.reported_at.replace(tzinfo=pytz.utc).astimezone(current_tz)
+        return '{datetime:%d-%m-%Y %H:%M:%S}'.format(datetime=reported_at)
 
     def serialize(self):
         return {
-            'username': self.user,
-            'happinnes': self.happy,
+            'username': self.name,
             'realname': self.realname,
-            'reported_at': ,
+            'happinnes': self.happy,
+            'reported_at': self.serialize_reported_at(),
             'comment': self.comment,
             'avatar': self.avatar
         }
