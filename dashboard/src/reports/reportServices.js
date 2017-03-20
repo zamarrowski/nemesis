735b3a6cbe9558201636ce44ce3a11e1d8582888
zamarrowski <sergiozamarro@hotmail.com>
2017-03-20 23:32:47 +0100

    Get user reports services

--- dashboard/src/reports/reportServices.js
+++ dashboard/src/reports/reportServices.js
@@ -5,5 +5,8 @@ import config from './../config'
 export default {
   getLastReports: () => {
     return axios.get(`${config.baseService}/last-reports/`)
+  },
+  getUserReports: (userId) => {
+    return axios.get(`${config.baseService}/last-reports/${userId}/`)
   }
 }
