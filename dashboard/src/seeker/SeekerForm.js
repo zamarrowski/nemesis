a4ffe01c217d40fb3b3501b410e7f91650753b32
zamarrowski <sergiozamarro@hotmail.com>
2017-03-21 22:11:28 +0100

    Show error when form is invalid

--- dashboard/src/seeker/SeekerForm.js
+++ dashboard/src/seeker/SeekerForm.js
@@ -6,6 +6,7 @@ import { List, ListItem } from 'material-ui/List'
 import Subheader from 'material-ui/Subheader'
 import Checkbox from 'material-ui/Checkbox'
 import Avatar from 'material-ui/Avatar'
+import Snackbar from 'material-ui/Snackbar'
 import styled from 'styled-components'
 import colors from './../common/colors'
 
@@ -15,6 +16,11 @@ const UserBox = styled.div`
 `
 
 class SeekerForm extends React.Component {
+
+  constructor() {
+    super()
+    this.state = { showError: false }
+  }
 
   render() {
 
@@ -29,52 +35,66 @@ class SeekerForm extends React.Component {
     const flatButtonStyles = { color: colors.main }
 
     return(
-      <Paper style={paperStyles} zDepth={1}>
-        <List>
-          <Subheader>Date range</Subheader>
-          <ListItem hoverColor="transparent">
-            <DatePicker
-              textFieldStyle={datePickerStyles}
-              hintText="Min date"
-              ref={(input) => { this._minDateInput = input }}
-              autoOk={true}
-            />
-          </ListItem>
-          <ListItem hoverColor="transparent">
-            <DatePicker
-              textFieldStyle={datePickerStyles}
-              hintText="Max date"
-              ref={(input) => { this._maxDateInput = input }}
-              autoOk={true}
-            />
-          </ListItem>
-        </List>
-        <List>
-         <Subheader>Users:</Subheader>
-         <UserBox>
-           {this.props.users.map((user, key) => (
-             <ListItem
-               primaryText={user.username} key={key}
-               leftCheckbox={
-                 <Checkbox iconStyle={{fill: colors.main}} onCheck={this._selectUser.bind(this, user)} />
-               }
-               rightAvatar={<Avatar src={user.avatar} />}
+      <div>
+        <Paper style={paperStyles} zDepth={1}>
+          <List>
+            <Subheader>Date range</Subheader>
+            <ListItem hoverColor="transparent">
+              <DatePicker
+                textFieldStyle={datePickerStyles}
+                hintText="Min date"
+                ref={(input) => { this._minDateInput = input }}
+                autoOk={true}
               />
-           ))}
-         </UserBox>
-       </List>
-        <FlatButton style={flatButtonStyles} onTouchTap={this._search.bind(this)} fullWidth={true}>Search</FlatButton>
-      </Paper>
+            </ListItem>
+            <ListItem hoverColor="transparent">
+              <DatePicker
+                textFieldStyle={datePickerStyles}
+                hintText="Max date"
+                ref={(input) => { this._maxDateInput = input }}
+                autoOk={true}
+              />
+            </ListItem>
+          </List>
+          <List>
+           <Subheader>Users:</Subheader>
+           <UserBox>
+             {this.props.users.map((user, key) => (
+               <ListItem
+                 primaryText={user.username} key={key}
+                 leftCheckbox={
+                   <Checkbox iconStyle={{fill: colors.main}} onCheck={this._selectUser.bind(this, user)} />
+                 }
+                 rightAvatar={<Avatar src={user.avatar} />}
+                />
+             ))}
+           </UserBox>
+         </List>
+          <FlatButton style={flatButtonStyles} onTouchTap={this._search.bind(this)} fullWidth={true}>Search</FlatButton>
+        </Paper>
+        <Snackbar
+          open={this.state.showError}
+          message="Min date, max date and users are required."
+          autoHideDuration={3000}
+        />
+      </div>
     )
   }
 
   _search() {
-    let searchInfo = {
-      minDate: this._minDateInput.state.date,
-      maxDate: this._maxDateInput.state.date,
-      users: this.props.users.filter(user => user.selected)
+    let usersSelected = this.props.users.filter(user => user.selected)
+    if (this._minDateInput.state.date && this._maxDateInput.state.date && usersSelected.length) {
+      let searchInfo = {
+        minDate: this._minDateInput.state.date,
+        maxDate: this._maxDateInput.state.date,
+        users: usersSelected
+      }
+      this.props.onSearch(searchInfo)
+      this.setState({ showError: false })
+    } else {
+      this.setState({ showError: true })
     }
-    this.props.onSearch(searchInfo)
+
   }
 
   _selectUser(userSelected) {
